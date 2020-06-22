4e867d649d3e2dc71101e465a61d05b03d765500
anin-chou-2020 <aninchou2020@outlook.in>
2020-06-22 22:46:27 +0530

    5 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule, AppRoutingModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule, AppRoutingModule ],
+  declarations: [ AppComponent, HeaderComponent, FooterComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
